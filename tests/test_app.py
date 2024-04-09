a1fd8530d9d86aefda64296d26b877a34a94ae46
SiyoungOh <81408668+siyoungoh@users.noreply.github.com>
2024-04-09 19:08:56 +0900

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import unittest
+from app.app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello_world(self):
+        response = self.client.get('/')
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
